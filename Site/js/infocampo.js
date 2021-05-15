b66cda0cdebf4cbb696656c7e782abbc0854c5d4
João Nunes <joaocarlospaivanunes@outlook.pt>
2021-05-15 20:14:59 +0100

    scipt php a mostrar os campos no mapa

--- Site/js/infocampo.js
+++ Site/js/infocampo.js
@@ -1,17 +1,26 @@
-var map = new ol.Map({
-    target: 'map',
-    layers: [
-      new ol.layer.Tile({
-        source: new ol.source.OSM()
-      })
-    ],
-    view: new ol.View({
-      center: ol.proj.fromLonLat([-8.65, 40.64]),
-      zoom: 13
-    })
-  });
 
+var map = new ol.Map({
+  target: 'map',
+  layers: [
+    new ol.layer.Tile({
+      source: new ol.source.OSM()
+    })
+  ],
+  view: new ol.View({
+    center: ol.proj.fromLonLat([-8.65, 40.64]),
+    zoom: 13
+  })
+});
+var entidadesSource = new ol.source.Vector({
+  url: "./php/infocampo.php",
+  format: new ol.format.GeoJSON()
+});
+var entidades = new ol.layer.Vector({
+  title: "Equipamentos desportivos de Aveiro",
+  source: entidadesSource
+});
+map.addLayer(entidades);
+// Add Vector layer to map
 function show(){
-    document.getElementById("sidebar").classList.toggle("active");
-    console.log("teste");
+  document.getElementById("sidebar").classList.toggle("active");
 }
