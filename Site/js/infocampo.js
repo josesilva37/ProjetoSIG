39a798c135d7b1d353a46ebd3359133ca88df206
João Nunes <joaocarlospaivanunes@outlook.pt>
2021-05-15 17:24:00 +0100

    info campo com openylayers

--- Site/js/infocampo.js
+++ Site/js/infocampo.js
@@ -1,3 +1,16 @@
+var map = new ol.Map({
+    target: 'map',
+    layers: [
+      new ol.layer.Tile({
+        source: new ol.source.OSM()
+      })
+    ],
+    view: new ol.View({
+      center: ol.proj.fromLonLat([37.41, 8.82]),
+      zoom: 4
+    })
+  });
+
 function show(){
     document.getElementById("sidebar").classList.toggle("active");
     console.log("teste");
