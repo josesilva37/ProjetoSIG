f21f00276b1b3c940fb5e09f10b9b9c405f0afeb
João Nunes <joaocarlospaivanunes@outlook.pt>
2021-05-15 19:01:21 +0100

    zoom inicial em portugal

--- Site/js/infocampo.js
+++ Site/js/infocampo.js
@@ -6,8 +6,8 @@ var map = new ol.Map({
       })
     ],
     view: new ol.View({
-      center: ol.proj.fromLonLat([37.41, 8.82]),
-      zoom: 4
+      center: ol.proj.fromLonLat([-7.51, 39.33]),
+      zoom: 7
     })
   });
 
