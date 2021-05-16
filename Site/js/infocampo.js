761abcaaea6a6d5d56cde07875a15e8f8dc3c508
João Nunes <joaocarlospaivanunes@outlook.pt>
2021-05-16 11:38:42 +0100

    pop de teste ao clicar no map

--- Site/js/infocampo.js
+++ Site/js/infocampo.js
@@ -1,5 +1,36 @@
 /*PARTE DO MAPA*/
 
+function show(){
+  document.getElementById("sidebar").classList.toggle("active");
+}
+
+/*Parte PopUp*/
+
+var container = document.getElementById('popup');
+var content = document.getElementById('popup-content');
+var closer = document.getElementById('popup-closer');
+
+
+var overlay = new ol.Overlay({
+  element: container,
+  autoPan: true,
+  autoPanAnimation: {
+    duration: 250,
+  },
+});
+
+closer.onclick = function () {
+  overlay.setPosition(undefined);
+  closer.blur();
+  return false;
+};
+
+/*//////////////////*/
+
+
+
+
+
 var map = new ol.Map({
   target: 'map',
   layers: [
@@ -7,11 +38,22 @@ var map = new ol.Map({
       source: new ol.source.OSM()
     })
   ],
+  overlays: [overlay],
   view: new ol.View({
     center: ol.proj.fromLonLat([-8.65, 40.64]),
     zoom: 13
   })
 });
+
+
+map.on('singleclick', function (evt) {
+  var coordinate = evt.coordinate;
+  var hdms = "teste";
+  content.innerHTML = '<p>You clicked here:</p><code>' + hdms + '</code>';
+  overlay.setPosition(coordinate);
+});
+
+
 var entidadesSource = new ol.source.Vector({
   url: "./php/infocampo.php",
   format: new ol.format.GeoJSON()
@@ -24,7 +66,5 @@ map.addLayer(entidades);
 /*//////////////////////////*/
 
 
+
 // Add Vector layer to map
-function show(){
-  document.getElementById("sidebar").classList.toggle("active");
-}
