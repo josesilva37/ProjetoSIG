15d1013173f8d9f6f221054d18f00315aec17b24
Tugabeast <goncalo.alves@ua.pt>
2021-05-16 01:26:36 +0100

    avaliarJogadores

---
+++ Site/js/avaliarJogadores.js
@@ -0,0 +1,72 @@
+var map = new ol.Map({
+    target: 'map',
+    layers: [
+      new ol.layer.Tile({
+        source: new ol.source.OSM()
+      })
+    ],
+    view: new ol.View({
+      center: ol.proj.fromLonLat([37.41, 8.82]),
+      zoom: 4
+    })
+  });
+
+function show(){
+    document.getElementById("sidebar").classList.toggle("active");
+    console.log("teste");
+}
+
+//pop up code
+
+var btnAdd = document.getElementById("btnAvaliarJogador")
+var span = document.getElementsByClassName("close")[0];
+var popup = document.getElementById("popup");
+
+
+
+btnAdd.onclick = function() {
+    popup.style.display = "block";
+}
+
+span.onclick = function() {
+    popup.style.display = "none";
+}
+
+window.onclick = function(event) {
+    if (event.target == popup) {
+        popup.style.display = "none";
+    }
+}
+
+//rating star
+
+const ratingStars = [...document.getElementsByClassName("rating__star")];
+const ratingResult = document.querySelector(".rating__result");
+
+printRatingResult(ratingResult);
+
+function executeRating(stars, result) {
+   const starClassActive = "rating__star fas fa-star";
+   const starClassUnactive = "rating__star far fa-star";
+   const starsLength = stars.length;
+   let i;
+   stars.map((star) => {
+      star.onclick = () => {
+         i = stars.indexOf(star);
+
+         if (star.className.indexOf(starClassUnactive) !== -1) {
+            printRatingResult(result, i + 1);
+            for (i; i >= 0; --i) stars[i].className = starClassActive;
+         } else {
+            printRatingResult(result, i);
+            for (i; i < starsLength; ++i) stars[i].className = starClassUnactive;
+         }
+      };
+   });
+}
+//print do numero de estrelas 0/5
+function printRatingResult(result, num = 0) {
+   //result.textContent = `${num}/5`;
+}
+
+executeRating(ratingStars, ratingResult);
