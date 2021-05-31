76677a5d3d0e2f795b150bf76dffe35dfbda0a03
josesilva37 <josesilva.jpas@gmail.com>
2021-05-31 10:35:33 +0100

    createAccount

--- Site/php/login.php
+++ Site/php/login.php
@@ -1,8 +1,12 @@
 <?php
-
+  ini_set('display_errors', 1);
 //$conn = new PDO('pgsql:host=gis4cloud.com;dbname=ptas2021_grupo1','ptas2021_grupo1','ptas2021_grupo1');
-
-$connection_string = "host={gis4cloud.com} port={5432} dbname={ptas2021_grupo1} user={ptas2021_grupo1} password={ptas2021_grupo1} ";
+$host = "gis4cloud.com";
+$port = "5432";
+$dbname = "ptas2021_grupo1";
+$user = "ptas2021_grupo1";
+$password = "ptas2021_grupo1"; 
+$connection_string = "host={$host} port={$port} dbname={$dbname} user={$user} password={$password} ";
 $dbconn = pg_connect($connection_string);
 
 if(isset($_POST['submit'])&&!empty($_POST['submit'])){
