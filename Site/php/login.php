8c640969677bca609f1059854aca1de1be869d67
josesilva37 <josesilva.jpas@gmail.com>
2021-06-01 15:09:01 +0100

    log in criar conta

--- Site/php/login.php
+++ Site/php/login.php
@@ -1,25 +1,24 @@
 <?php
-  ini_set('display_errors', 1);
+ini_set('display_errors', 1);
 //$conn = new PDO('pgsql:host=gis4cloud.com;dbname=ptas2021_grupo1','ptas2021_grupo1','ptas2021_grupo1');
 $host = "gis4cloud.com";
 $port = "5432";
 $dbname = "ptas2021_grupo1";
 $user = "ptas2021_grupo1";
-$password = "ptas2021_grupo1"; 
+$password = "ptas2021_grupo1";
 $connection_string = "host={$host} port={$port} dbname={$dbname} user={$user} password={$password} ";
 $dbconn = pg_connect($connection_string);
 
-if(isset($_POST['submit'])&&!empty($_POST['submit'])){
-    $hashpassword = md5($_POST['psw']);
-    $sql ="select *from public.utilizador where email = '".pg_escape_string($_POST['email'])."' and pass ='".$hashpassword."'";
-    $data = pg_query($dbconn,$sql);
-    $login_check = pg_num_rows($data);
-    if($login_check > 0){ 
-        
-        echo "Login Successfully";    
-    }else{
-        
-        echo "Invalid Details";
-    }
+
+$hashpassword = md5($_POST['psw']);
+$sql = "select *from public.utilizador where email = '" . pg_escape_string($_POST['email']) . "' and pass ='" . $hashpassword . "'";
+$data = pg_query($dbconn, $sql);
+$login_check = pg_num_rows($data);
+if ($login_check > 0) {
+
+    echo "Login Successfully";
+} else {
+
+    echo "Invalid Details";
 }
 ?>
