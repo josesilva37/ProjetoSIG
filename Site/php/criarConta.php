21cdb0167f3ab1dfc5c7d2d7f90e75d80db222d8
josesilva37 <josesilva.jpas@gmail.com>
2021-07-03 11:38:05 +0100

    criar conta verifição

--- Site/php/criarConta.php
+++ Site/php/criarConta.php
@@ -1,15 +1,22 @@
 <?php
 ini_set('display_errors', 1);
 include('conexao.php');
+header('Content-Type: application/json');
+
+$data = json_decode($_POST['json']);
+$username = $data->username;
+$email = $data->email;
+$pass = $data->pass;
+
 try{
         global $pdo;
-        $q = "INSERT INTO utilizador(username,email,pass)values('" . $_POST['username'] . "','" . $_POST['email'] . "','" . md5($_POST['password']) . "')";
+        $q = "INSERT INTO utilizador(username,email,pass)values('" . $username . "','" . $email . "','" . md5($pass) . "')";
         $stmt = $pdo->prepare($q);
         $stmt->execute();
-
-        header("Location: ../index.php");
+        echo json_encode("Sucesso");
+        //header("Location: ../index.php");
         
     }catch(PDOException $e){
-        echo $e->getMessage();
+        echo json_encode($e->getMessage());
     }
 ?>
